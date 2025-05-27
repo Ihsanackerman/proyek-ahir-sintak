5650646dda1ebdaf83dd75bcf5a177b48265eac8
ihsanackerman <musannjenk@gmail.com>
2025-05-27 11:26:50 +0700

    gnhjm

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { Link } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
+      <HomePage/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,38 +1,10 @@
 import React, { useState } from "react";
 import { db } from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
-
 import React from "react";
 
 function ContactPage() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Simpan data ke Firestore
-      await addDoc(collection(db, "contacts"), formData);
-      alert("Pesan berhasil dikirim!");
-      setFormData({ name: "", email: "", message: "" }); // Reset form
-    } catch (error) {
-      console.error("Error submitting form: ", error);
-      alert("Terjadi kesalahan. Silakan coba lagi.");
-    }
-  };
-
-  return <div>ContactPage</div>;
+  return <></>;
 }
 
 export default ContactPage;
