01e8b49608671b3342275dd61c7423c6546de588
ihsanackerman <musannjenk@gmail.com>
2025-05-27 11:33:19 +0700

    bismillah

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { db } from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
 import React from "react";
+import NavBar from "./NavBar";
 
 function ContactPage() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default ContactPage;
---
+++ src/Pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function ProductPage() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default ProductPage;
